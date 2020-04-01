a5ed5d4a1acffe6f70607b555bfed609598d2c7a
Preston Carroll <plutusinc1@yahoo.com>
2020-04-01 16:32:52 -0500

    fixed Enum.chunk to chunk.every   now working

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,44 @@ def main(input) do
   |> pick_color
   |> build_grid
   |> filter_odd_squares
+  |> build_pixel_map
+  |> draw_image
+  |> save_image(input)
+
+end
+
+def save_image(image, input) do
+  File.write("#{input}.png", image)
+
+end
+
+
+def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+
+    Enum.each pixel_map, fn({start, stop}) -> 
+        :egd.filledRectangle(image, start, stop, fill)
+
+
+    end
+
+    :egd.render(image)
+end
+
+
+def build_pixel_map(%Identicon.Image{grid: grid} = image)  do
+  pixel_map = Enum.map grid, fn({_code, index}) ->
+    horizontal = rem(index, 5) * 50
+    vertical = div(index,5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+  end
+  %Identicon.Image{image | pixel_map: pixel_map}
 
 end
 
@@ -26,7 +64,7 @@ end
 
 def build_grid(%Identicon.Image{hex: hex} = image) do
   grid = hex
-    |> Enum.chunk(3)
+    |> Enum.chunk_every(3, 3, :discard)
     |> Enum.map(&mirror_row/1)
     |> List.flatten
     |> Enum.with_index
@@ -40,7 +78,7 @@ end
 def mirror_row(row) do
   [first, second | _tail] = row
 
-  row ++ [second,first]
+  row ++ [second, first]
 
 
 end
--- lib/image.ex
+++ lib/image.ex
@@ -1,6 +1,6 @@
 defmodule Identicon.Image do
 
-defstruct hex: nil, color: nil, grid: nil
+defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 
 
 
